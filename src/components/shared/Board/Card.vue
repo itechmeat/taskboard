71983ac2b8ddc971bccfda8b708eeaabb6bc6bbb
Sergey Eroshenkov <itechmeat@gmail.com>
2020-11-25 16:45:30 +0300

    Add progress to cards

--- src/components/shared/Board/Card.vue
+++ src/components/shared/Board/Card.vue
@@ -1,7 +1,10 @@
 <template>
   <li v-if="value" class="board-card">
     <router-link class="board-card__link" :to="'/project/' + value.id">
-      {{ value.name }}
+      <div class="board-card__label">{{ value.name }}</div>
+      <div class="board-card__scale">
+        <div class="board-card__progress" :style="progressStyles"></div>
+      </div>
     </router-link>
   </li>
 </template>
@@ -16,18 +19,55 @@ export default {
       required: true,
     },
   },
+
+  computed: {
+    progressStyles() {
+      if (!this.value) {
+        return;
+      }
+
+      const progress = this.value.progress || 0;
+      let background;
+
+      switch (true) {
+        case progress > 25 && progress < 50:
+          background = "var(--color-warning)";
+          break;
+        case progress >= 50 && progress < 75:
+          background = "var(--color-accent)";
+          break;
+        case progress >= 75 && progress < 100:
+          background = "var(--color-primary)";
+          break;
+        case progress === 100:
+          background = "var(--color-success)";
+          break;
+        default:
+          background = "var(--color-danger)";
+      }
+
+      return {
+        height: progress + "%",
+        background,
+      };
+    },
+  },
 };
 </script>
 
 <style lang="scss">
+@import "@/styles/system";
+
 $block: ".board-card";
 
 #{$block} {
   font-size: var(--font-size-base);
 
   &__link {
+    overflow: hidden;
     display: block;
-    padding: var(--gap);
+    position: relative;
+    padding: var(--gap) calc(var(--gap) - 4px);
     border-radius: var(--border-raius);
     background: var(--color-light);
     box-shadow: 0 2px 0 -1px rgba(#000, 0.1);
@@ -39,6 +79,23 @@ $block: ".board-card";
       transform: translateY(-1px);
       box-shadow: 0 4px 2px 1px rgba(#000, 0.1);
     }
+  }
+
+  &__scale {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    width: 4px;
+    background: var(--color-bg);
+  }
+
+  &__progress {
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background: var(--color-text);
   }
 }
 </style>
--- src/components/shared/Board/index.vue
+++ src/components/shared/Board/index.vue
@@ -28,51 +28,90 @@ export default {
             {
               id: "c1",
               name: "Card Number 1",
+              progress: 0,
             },
             {
               id: "c2",
               name: "Card Number 2",
+              progress: 0,
             },
           ],
         },
         {
           id: "2",
           name: "To Do",
-          cards: [],
+          cards: [
+            {
+              id: "c3",
+              name: "Card Number 3",
+              progress: 67,
+            },
+            {
+              id: "c4",
+              name: "Card Number 4",
+              progress: 84,
+            },
+            {
+              id: "c5",
+              name: "Card Number 5",
+              progress: 25,
+            },
+            {
+              id: "c6",
+              name: "Card Number 6",
+              progress: 10,
+            },
+            {
+              id: "c7",
+              name: "Card Number 7",
+              progress: 38,
+            },
+          ],
         },
         {
           id: "3",
           name: "In Progress",
           cards: [
             {
-              id: "c3",
-              name: "Card Number 3",
+              id: "c8",
+              name: "Card Number 8",
+              progress: 97,
             },
           ],
         },
         {
           id: "4",
+          name: "Testing",
+          cards: [],
+        },
+        {
+          id: "5",
           name: "Done",
           cards: [
             {
-              id: "c4",
-              name: "Card Number 4",
+              id: "c9",
+              name: "Card Number 9",
+              progress: 100,
             },
             {
-              id: "c5",
-              name: "Card Number 5",
+              id: "c10",
+              name: "Card Number 10",
+              progress: 100,
             },
             {
-              id: "c6",
-              name: "Card Number 6",
+              id: "c11",
+              name: "Card Number 11",
+              progress: 100,
             },
             {
-              id: "c7",
-              name: "Card Number 7",
+              id: "c12",
+              name: "Card Number 12",
+              progress: 100,
             },
             {
-              id: "c8",
-              name: "Card Number 8",
+              id: "c13",
+              name: "Card Number 13",
+              progress: 100,
             },
           ],
         },
