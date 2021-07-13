ed3a7373bb316bf01b0cdd77f434b84fd5bdf06c
Andrey Yarysh <and@yarysh.com>
2021-07-13 22:43:19 +0200

    Add dummy test

--- server/controllers/user_test.go
+++ server/controllers/user_test.go
@@ -1,5 +1,28 @@
 package controllers
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
 
-func TestSignUp(t *testing.T) {}
+	"github.com/labstack/echo/v4"
+)
+
+func TestSignUp(t *testing.T) {
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/signup", nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	h := &UserHandler{}
+
+	if err := h.SignUp(c); err != nil {
+		t.Errorf("get error: %s", err)
+		return
+	}
+
+	want := "Hello, World!"
+	got := rec.Body.String()
+	if want != got {
+		t.Errorf("want: %s got: %s", want, got)
+	}
+}
